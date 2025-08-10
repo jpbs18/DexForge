da4f685184c63c0d4b06135680a57549d826d91d
Joao <j.p.santos21@hotmail.com>
2025-08-10 15:45:04 +0100

    Fixed title padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           />
         </div>
         <div className="md:text-left max-w-md">
-          <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-4 bg-gradient-to-r from-indigo-500 to-pink-500 text-transparent bg-clip-text px-2">
+          <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-4 bg-gradient-to-r from-indigo-500 to-pink-500 text-transparent bg-clip-text py-2">
              Welcome to DexForge!
           </h1>
           <p className="text-lg md:text-xl text-gray-700 px-2">
