848c5dfc6d37e415d3759d4b6cc2b579a594f983
Joao <j.p.santos21@hotmail.com>
2025-08-11 20:30:30 +0100

    Fixed performance issue to fecth all pokemons on Pokedex page

--- src/components/pokemon/PokemonList.tsx
+++ src/components/pokemon/PokemonList.tsx
@@ -1,65 +1,45 @@
 "use client";
 
-import { useCallback, useState } from "react";;
+import { useEffect, useState } from "react";;
 import { PokemonCard } from "./PokemonCard";
 import { PokeData, Pokemon } from "@/models/pokemon";
-
-const fetchNextPokemonOffset = async (offset: number, limit: number) => {
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`);
-    const data = await res.json();
-
-    return await Promise.all(
-        data.results.map(async (pokemon: PokeData) => {
-            const res = await fetch(pokemon.url);
-            return await res.json();
-        })
-    );
-}
+import { get, set } from "idb-keyval";
 
 export default function PokemonList({ initialData }: { initialData: Pokemon[] }) {
   const [pokemons, setPokemons] = useState<Pokemon[]>(initialData);
-  const [offset, setOffset] = useState(12);
-  const [loading, setLoading] = useState(false);
-  const [hasMore, setHasMore] = useState(true);
 
-  const loadMore = useCallback(async () => {
-    if (loading || !hasMore) return;
+  useEffect(() => {
+    (async () => {
+      try {
+        const cached = await get<Pokemon[]>("fullPokemonList");
+        if (cached && cached.length > 0) {
+          setPokemons(cached);
+          return;
+        }
 
-    const remaining = 1025 - pokemons.length;
-    if (remaining == 0) {
-      setHasMore(false);
-      return;
-    }
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=1025`);
+        const data = await res.json();
 
-    const limit = Math.min(12, remaining);
-    setLoading(true);
+        const fullList: Pokemon[] = await Promise.all(
+          data.results.map(async (p: PokeData) => {
+            const detailRes = await fetch(p.url);
+            return detailRes.json();
+          })
+        );
 
-    const pokemonsList = await fetchNextPokemonOffset(offset, limit);
-    if (remaining === 0) {
-      setHasMore(false);
-    } else {
-      setPokemons((prev) => [...prev, ...pokemonsList]);
-      setOffset((prev) => Math.min(prev + 12, 1025));
-    }
-
-    setLoading(false);
-  }, [loading, hasMore, offset, pokemons.length]);
-
+        setPokemons(fullList);
+        await set("fullPokemonList", fullList);
+      } catch (err) {
+        console.error("Error fetching all Pokémon:", err);
+      }
+    })();
+  }, []);
+  
   return (
     <div>
       <ul className="grid sm:grid-cols-3 lg:grid-cols-4 gap-4">
         {pokemons.map((pokemon: Pokemon) => <PokemonCard key={pokemon.id} pokemon={pokemon} />)}
       </ul>
-      <div className="flex justify-center mt-6">
-        <button
-          onClick={loadMore}
-          disabled={loading || !hasMore}
-          className={`bg-[#be0000] hover:bg-[#f80101] text-white px-4 py-2 rounded mt-2 cursor-pointer
-            ${loading || !hasMore ? "opacity-50 cursor-not-allowed hover:bg-indigo-500" : ""}`}
-          >
-          {loading ? "Loading..." : hasMore ? "Load More" : "No more Pokémon"}
-        </button>
-      </div>
     </div>
   );
 }
