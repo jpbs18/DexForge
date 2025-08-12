e2c4d7c17f04ed845963a33200f33f0fd7559874
Joao <j.p.santos21@hotmail.com>
2025-08-12 20:23:23 +0100

    Fixing button for loading pokemons

--- src/components/pokemon/PokemonList.tsx
+++ src/components/pokemon/PokemonList.tsx
@@ -6,9 +6,13 @@ import { Pokemon } from "@/models/pokemon";
 
 export default function PokemonList({ initialData }: { initialData: Pokemon[] }) {
   const [visibleCount, setVisibleCount] = useState(12);
+  const [loading, setLoading] = useState(false);
+
 
   const handleLoadMore = () => {
+    setLoading(true);
     setVisibleCount((prev) => Math.min(prev + 12, initialData.length));
+    setLoading(false);
   };
   
   return (
@@ -17,8 +21,8 @@ export default function PokemonList({ initialData }: { initialData: Pokemon[] })
         {initialData.slice(0, visibleCount).map((pokemon: Pokemon) => <PokemonCard key={pokemon.id} pokemon={pokemon} />)}
       </ul>
       {visibleCount < initialData.length && (
-        <button onClick={handleLoadMore} className="mt-4 px-4 py-2 bg-blue-600 text-white rounded">
-          Load More
+        <button onClick={handleLoadMore} disabled={loading} className="bg-[#be0000] hover:bg-[#f80101] text-white px-4 py-2 mt-2 rounded-md block mx-auto transition-colors">
+          {loading ? "Loading..." : "Load More"}
         </button>
       )}
     </div>
