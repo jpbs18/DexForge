a6415e06243df82f7466fd009ec4e8fda244d1cd
Joao <j.p.santos21@hotmail.com>
2025-08-14 19:35:37 +0100

    Removed property from card

--- src/components/pokemon/PokemonCard.tsx
+++ src/components/pokemon/PokemonCard.tsx
@@ -1,51 +1,54 @@
 import { Pokemon } from "@/models/pokemon";
 import { typeColors } from "@/utils/type-colors";
 import Image from "next/image";
+import Link from "next/link";
 
 export function PokemonCard({ pokemon }: { pokemon: Pokemon }) {
   const primaryType = pokemon.types[0];
 
   return (
-    <li
-      className={`rounded-xl shadow-md overflow-hidden flex flex-col items-center mx-auto duration-300 
+    <Link href={`/pokedex/${pokemon.id}`} className="w-full max-w-[250px]">
+      <li
+        className={`rounded-xl shadow-md overflow-hidden flex flex-col items-center mx-auto duration-300 
         hover:rotate-[3deg] hover:shadow-2xl animate-fade-slide-up hover:shadow-black/40 dark:hover:shadow-white/20
       bg-white dark:bg-gray-800 border-2 border-indigo-100 cursor-pointer max-w-[250px] w-full`}
-    >
-      <div className="w-full bg-gray-800 text-white px-4 py-2 font-bold uppercase tracking-wide flex justify-between items-center">
-        <span className="capitalize text-sm sm:text-base">
-          {pokemon.name.length >= 12
-            ? pokemon.name.split("-")[0]
-            : pokemon.name}
-        </span>
-        <span className="text-xs sm:text-sm">
-          #{pokemon.id.toString().padStart(3, "0")}
-        </span>
-      </div>
-      <div
-        className={`relative p-6 flex flex-col items-center w-full ${typeColors[primaryType]}`}
       >
-        <div className="absolute inset-0 opacity-10 bg-[url('/pokeball-bg.svg')] bg-center bg-no-repeat bg-contain" />
-        <div className="w-40 h-40 relative mb-4 z-10">
-          <Image
-            src={pokemon.front_default}
-            alt={pokemon.name}
-            fill
-            style={{ objectFit: "contain" }}
-            sizes="160px"
-            loading="lazy"
-          />
+        <div className="w-full bg-gray-800 text-white px-4 py-2 font-bold uppercase tracking-wide flex justify-between items-center">
+          <span className="capitalize text-sm sm:text-base">
+            {pokemon.name.length >= 12
+              ? pokemon.name.split("-")[0]
+              : pokemon.name}
+          </span>
+          <span className="text-xs sm:text-sm">
+            #{pokemon.id.toString().padStart(3, "0")}
+          </span>
         </div>
-        <div className="flex gap-2 z-10">
-          {pokemon.types.map((type) => (
-            <span
-              key={type}
-              className={`text-xs font-semibold px-2 py-1 w-16 text-center rounded border-2 border-white/50 ${typeColors[type]} bg-opacity-80 backdrop-brightness-200 shadow-lg`}
-            >
-              {type}
-            </span>
-          ))}
+        <div
+          className={`relative p-6 flex flex-col items-center w-full ${typeColors[primaryType]}`}
+        >
+          <div className="absolute inset-0 opacity-10 bg-[url('/pokeball-bg.svg')] bg-center bg-no-repeat bg-contain" />
+          <div className="w-40 h-40 relative mb-4 z-10">
+            <Image
+              src={pokemon.front_default}
+              alt={pokemon.name}
+              fill
+              style={{ objectFit: "contain" }}
+              sizes="160px"
+              loading="lazy"
+            />
+          </div>
+          <div className="flex gap-2 z-10">
+            {pokemon.types.map((type) => (
+              <span
+                key={type}
+                className={`text-xs font-semibold px-2 py-1 w-16 text-center rounded border-2 border-white/50 ${typeColors[type]} bg-opacity-80 backdrop-brightness-200 shadow-lg`}
+              >
+                {type}
+              </span>
+            ))}
+          </div>
         </div>
-      </div>
-    </li>
+      </li>
+    </Link>
   );
 }
