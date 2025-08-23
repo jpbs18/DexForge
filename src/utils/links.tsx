f8c9e9089657ceb52517ebc5c4c89e2c0b4e3c0a
Joao <j.p.santos21@hotmail.com>
2025-08-23 21:42:53 +0100

    Changed logo

--- src/utils/links.tsx
+++ src/utils/links.tsx
@@ -1,11 +1,10 @@
 import { FaHome, FaGamepad } from "react-icons/fa";
-import { SiPokemon } from "react-icons/si";
-import { GiPokecog, GiNewspaper } from "react-icons/gi";
+import { GiPokecog, GiNewspaper, GiNotebook } from "react-icons/gi";
 import { MdOutlinePeople } from "react-icons/md";
 
 export const navLinks = [
   { label: "Home", path: "/", icon: FaHome },
-  { label: "Pokédex", path: "/pokedex", icon: SiPokemon },
+  { label: "Pokédex", path: "/pokedex", icon: GiNotebook },
   { label: "PokéBuilder", path: "/poke-builder", icon: GiPokecog },
   { label: "Games", path: "/games", icon: FaGamepad },
   { label: "News", path: "/news", icon: GiNewspaper },
